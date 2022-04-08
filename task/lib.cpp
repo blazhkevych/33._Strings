874400181fb0aa51f7873e838174cfa40acdd026
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-04-08 21:45:36 +0300

    Add project files.

---
+++ task/lib.cpp
@@ -0,0 +1,91 @@
+﻿#include<windows.h>
+#include<iostream>
+using namespace std;
+
+// Функция заменяет в строке все пробелы на табуляции
+char* ReplaceByTabSpace(char* str)
+{
+	for (int i = 0; str[i] != '\0'; i++)
+		if (str[i] == ' ')
+			str[i] = '\t';
+	return str;
+}
+
+// Функция подсчитывает количество слов во введенном предложении
+int CountWordInString(const char* str)
+{
+	int count = 0;
+	for (int i = 0; str[i] != '\0'; i++)
+		if (str[i] != ' ' && str[i] != '\t' && (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t'))
+			count++;
+	return count;
+}
+
+// Функция определяет длину строки
+int mystrlen(const char* s)
+{
+	int counter = 0;
+	for (int i = 0; s[i] != '\0'; i++)
+		counter++;
+	return counter;
+}
+
+// Функция подсчитывает количество гласных букв в русском тексте
+int NumberOfVowelsInString(const char* str)
+{
+	int counter = 0;
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == 'а' || str[i] == 'е' || str[i] == 'ё' ||
+			str[i] == 'и' || str[i] == 'о' || str[i] == 'у' ||
+			str[i] == 'ы' || str[i] == 'э' || str[i] == 'ю' || str[i] == 'я')
+			counter++;
+	}
+	return counter;
+}
+
+// Функция выводит русский текст в окно консоли
+void RussianMessage(const char* s)
+{
+	char* buf = new char[mystrlen(s) + 1];
+	CharToOemA(s, buf);
+	cout << buf;
+	delete[] buf;
+}
+
+// Функция копирует строку str2 в буфер, адресуемый через str1.
+// Функция возвращает указатель на первую строку str1.
+char* mystrcpy(char* s1, const char* s2)
+{
+	int i;
+	for (i = 0; s2[i] != 0; i++)
+		s1[i] = s2[i];
+	s1[i] = '\0';
+	return s1;
+}
+
+// Функция присоединяет строку str2 к строке str1.
+// Функция возвращает указатель на первую строку str1.
+char* mystrcat(char* s1, const char* s2)
+{
+	char* p = s1;
+	while (*p != '\0')
+		p++;
+	int i;
+	for (i = 0; s2[i] != '\0'; i++)
+		p[i] = s2[i];
+	p[i] = '\0';
+	return s1;
+}
+
+// Функция осуществляет поиск символа s в строке str. 
+// Функция возвращает указатель на первое вхождение символа в строку, в противном случае 0.
+char* mystrchr(char* s1, char s)
+{
+	for (int i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] == s)
+			return s1 + i;
+	}
+	return nullptr;
+}
---
+++ task/task.cpp
@@ -0,0 +1,108 @@
+﻿#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main()
+{
+	char answer{ 0 };
+	int choice{ 0 };
+
+	do
+	{
+		cout << "Enter task number:: "
+			<< "\nTask 1."
+			<< "\nTask 2."
+			<< "\nTask 3."
+			<< "\nTask 4."
+			<< "\nTask 5."
+			<< "\nTask 6."
+			<< "\nTask 7."
+			<< endl;
+		cin >> choice;
+
+		switch (choice)
+		{
+		case 1:
+		{
+			/*
+			a. char* mystrstr (char* str1, char* str2); - функция осуществляет
+			поиск подстроки str2 в строке str1. Функция возвращает
+			указатель на первое вхождение подстроки str2 в строку str1, в
+			противном случае 0.
+			*/
+
+		}
+		break;
+
+		case 2:
+		{
+			/*
+			b. int mystrcmp (const char* str1, const char* str2); - функция
+			сравнивает две строки, и, если строки равны возвращает 0,
+			если первая строка больше второй, то возвращает 1, иначе -1.
+			*/
+
+		}
+		break;
+
+		case 3:
+		{
+			/*
+			c. int StringToNumber (char* str); - функция конвертирует строку
+			в число и возвращает это число.
+			*/
+
+		}
+		break;
+
+		case 4:
+		{
+			/*
+			d. char* NumberToString (int number); - функция конвертирует
+			число в строку и возвращает указатель на эту строку.
+			*/
+
+		}
+		break;
+
+		case 5:
+		{
+			/*
+			e. char* Uppercase (char* str1); - функция преобразует строку в
+			верхний регистр.
+			*/
+
+		}
+		break;
+
+		case 6:
+		{
+			/*
+			f. char* Lowercase (char* str1); - функция преобразует строку в
+			нижний регистр.
+			*/
+
+		}
+		break;
+
+		case 7:
+		{
+			/*
+			g. char* mystrrev (char* str); - функция реверсирует строку и
+			возвращает указатель на новую строку.
+			*/
+
+		}
+		break;
+		}
+
+		cout << endl;
+		cout << "Do you want to continue? ( y (yes) / n (no) )\n";
+		cin >> answer;
+		cout << endl;
+
+	} while (answer == 'y');
+
+	return 0;
+}
